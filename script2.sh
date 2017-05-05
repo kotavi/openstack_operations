c04e2a9ac2e35f9668dd8679438eb371c2e3da8d
Maxim Lvov <mlvov@mirantis-apple.local>
2017-05-04 18:52:44 -0700

    new scripts

---
+++ script2.sh
@@ -0,0 +1,19 @@
+
+# Scenario 1
+# read image name
+# create volume from image
+# create snapshot from volume
+# create security group
+# create keypair
+# launch VM from snapshot with keypair  and get hostname of VM
+
+# Scenario 2
+# read image name
+# create volume from image
+# create security group
+# create keypair
+# launch VM from volume and get hostname of VM
+# create snapshot from volume
+# create security group
+# create keypair
+# launch VM from snapshot with keypair and get hostname of VM
