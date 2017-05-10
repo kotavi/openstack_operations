9c9d7b78249e3c8697e0928f13df735d4bd9280d
Tetiana Korchak <tkorchak@mirantis.com>
2017-05-10 00:16:22 -0700

    fixes

--- scripts/upload_image.sh
+++ scripts/upload_image.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -x
 
-#wget https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img
-#wget http://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img
+#https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img
+#http://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img
 #http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
+#http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img
+#http://cdimage.debian.org/cdimage/openstack/current-8/debian-8-openstack-amd64.qcow2
+
 image_name=$1
 image_link=$2
 
