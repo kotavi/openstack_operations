88ce9ca7831a2bc059511fd12f6abca432e11abf
Tetiana Korchak <tkorchak@mirantis.com>
2017-05-09 20:06:00 -0700

    new scripts

--- scripts/upload_image.sh
+++ scripts/upload_image.sh
@@ -2,9 +2,13 @@
 
 #wget https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img
 #wget http://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img
-wget http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
+#http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
+image_name=$1
+image_link=$2
+
+wget -O $image_name image_link
 
 
-openstack image create --disk-format qcow2 --min-ram 1500 --file ubuntu-16.04-server-cloudimg-amd64-disk1.img xenial16.04
+openstack image create --disk-format qcow2 --min-ram 1500 --file $image_name $image_name
 
-openstack image list | grep xenial16.04
+openstack image list | grep $image_name
